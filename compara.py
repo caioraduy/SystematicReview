be77e327f7a885b9de6a5646c7cad788f5cdb984
caioraduy <raduycaio@gmail.com>
2024-02-03 18:04:17 -0300

    ADICIONA COMENTÁRIOS

--- compara.py
+++ compara.py
@@ -33,48 +33,38 @@ def exporta_lista_para_csv(sua_lista, nome_arquivo):
 
 
 if __name__== '__main__':
+    #importa os arquivos CSV
     ref01 = classificar_por_coluna("C:\\Users\\raduy\\PycharmProjects\\pythonProject12\\SCOPUS2024.csv", "Title")
     ref02 = classificar_por_coluna("C:\\Users\\raduy\\PycharmProjects\\pythonProject12\\SCOPUS2021.csv", "Title")
-
+    #croa lista com o título
     lista01 = ref01["Title"]
     lista02 = ref02["Title"]
 
-    print(len(lista01))
-    print(len(lista02))
 
+    # cria dicionário vazio
     TT = {}
-    x=0
+    # faz o for na lista 1
     for k in lista01:
-        x = x+1
         k = limpar_frase(k).lower()
-        #print(x, k)
+        # verifica o número de vezes que esse título existe e adiciona no dicionário
         if k not in TT:
             TT[k] = {"LISTA1": 1}
         else:
             TT[k]["LISTA1"] = TT[k]["LISTA1"] + 1
-    print(x)
-    j = 0
-    for k, v in TT.items():
-        j = j + 1
-        #print(j, v, k)
-
+    # faz o for na lista 2
     for k in lista02:
         k = limpar_frase(k).lower()
+        # verifica se a lista 2 está presente no dicionário e adiciona qntas vezes est[a
         if k not in TT:
             TT[k] = {"LISTA2": 1}
         else:
             if "LISTA2" not in TT[k]:
                 TT[k]["LISTA2"] = 0
             TT[k]["LISTA2"] = TT[k]["LISTA2"] + 1
-
+    # cria lista vazia para adicionar os arquivos
     lista_novos_artigos = []
-    i = 0
-    j=0
-    print(TT)
+    # se está na lista 1 e não está na 2 trata-se de um artigo novo
     for k, v in TT.items():
-        j = j + 1
-        #print(j, v, k)
-
         if "LISTA1" in v and "LISTA2" not in v:
             for i in range(0, TT[k]['LISTA1']):
                 artigo =[]
@@ -82,11 +72,12 @@ if __name__== '__main__':
                 lista_novos_artigos.append(artigo)
             print(TT[k]['LISTA1'])
             i += TT[k]['LISTA1']
-
+    # se está na lista 1 e não está na 2 trata-se de um artigo que não aparece mais na string de busca
     for k, v in TT.items():
         if "LISTA2" in v and "LISTA1" not in v:
             i -= TT[k]['LISTA2']
-
+    # se o mesmo nome aparece 2 vezes na base 1 e passa a aparecer 4.
+    #  O artigo aumentou o número de repetições
     for k,v in TT.items():
         if "LISTA2" in v and "LISTA1" in v:
             atigos_que_aumenta_o_numero_de_repeticoes = TT[k]["LISTA1"] - TT[k]['LISTA2']
@@ -98,7 +89,7 @@ if __name__== '__main__':
                     artigo.append(k)
                     lista_novos_artigos.append(artigo)
 
-            #print('xj')
+
 
 
 
