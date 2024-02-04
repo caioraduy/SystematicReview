44cc11ea2e2a9f13fb7a349f7dfae7076c02cca8
caioraduy <raduycaio@gmail.com>
2024-02-03 23:51:44 -0300

    TENTA ADICIONAR ANO

--- compara.py
+++ compara.py
@@ -31,68 +31,79 @@ def exporta_lista_para_csv(sua_lista, nome_arquivo):
 
     print(f'A lista foi exportada para o arquivo CSV: {nome_arquivo}')
 
+def adiciona_artigo_na_lista(lista, numero_elementos, artigo):
+    for i in range(0, numero_elementos):
+        artigo_lista = []
+        artigo_lista.append(artigo)
+        lista.append(artigo)
+    return lista
+
+def compara_duas_listas(lista01,lista02):
+    TT = {}
+    # faz o for na lista 1
+    for i in range(0,len(lista01)):
+        artigo = limpar_frase(lista01[i][0]).lower()
+        # verifica o número de vezes que esse título existe e adiciona no dicionário
+        if artigo not in TT:
+            TT[artigo] = {"LISTA1": 1, "ANO": lista01[i][1]}
+        else:
+            TT[artigo]["LISTA1"] = TT[artigo]["LISTA1"] + 1
+    # faz o for na lista 2
+    for i in range(0, len(lista02)):
+        artigo = limpar_frase(lista02[i][0]).lower()
+        # verifica se a lista 2 está presente no dicionário e adiciona qntas vezes est[a
+        if artigo not in TT:
+            TT[artigo] = {"LISTA2": 1,"ANO": lista02[i][1]}
+        else:
+            if "LISTA2" not in TT[artigo]:
+                TT[artigo]["LISTA2"] = 0
+            TT[artigo]["LISTA2"] = TT[artigo]["LISTA2"] + 1
+    print(TT)
+    return TT
+
+def retorna_lista_delta(TT):
+    i=0
+    lista_novos_artigos = []
+    # se está na lista 1 e não está na 2 trata-se de um artigo novo
+    for k, v in TT.items():
+        if "LISTA1" in v and "LISTA2" not in v:
+            lista_novos_artigos= adiciona_artigo_na_lista(lista_novos_artigos, TT[k]['LISTA1'],k)
+            i += TT[k]['LISTA1']
+
+    # se está na lista 1 e não está na 2 trata-se de um artigo que não aparece mais na string de busca
+    for k, v in TT.items():
+        if "LISTA2" in v and "LISTA1" not in v:
+            print(f'O artigo com o título "{k}" deixa de existir na string de busca')
+            i -= TT[k]['LISTA2']
+    # se o mesmo nome aparece 2 vezes na base 1 e passa a aparecer 4.
+    #  O artigo aumentou o número de repetições
+    for k, v in TT.items():
+        if "LISTA2" in v and "LISTA1" in v:
+            artigos_que_aumenta_o_numero_de_repeticoes = TT[k]["LISTA1"] - TT[k]['LISTA2']
+            if artigos_que_aumenta_o_numero_de_repeticoes > 0:
+                print(f'O artigo com o título "{k}" deve ser contabilizado {artigos_que_aumenta_o_numero_de_repeticoes} vezes, '
+                      f'pq o número de repetições aumentou ')
+
+                i += artigos_que_aumenta_o_numero_de_repeticoes
+                lista_novos_artigos = adiciona_artigo_na_lista(lista_novos_artigos, artigos_que_aumenta_o_numero_de_repeticoes, k)
+
+    print(f'O delta entre as lista é {i}')
+    return lista_novos_artigos
+
+
 
 if __name__== '__main__':
     #importa os arquivos CSV
     ref01 = classificar_por_coluna("C:\\Users\\raduy\\PycharmProjects\\pythonProject12\\SCOPUS2024.csv", "Title")
     ref02 = classificar_por_coluna("C:\\Users\\raduy\\PycharmProjects\\pythonProject12\\SCOPUS2021.csv", "Title")
     #croa lista com o título
-    lista01 = ref01["Title"]
-    lista02 = ref02["Title"]
 
+    colunas_selecionadas = ["Title", "Year"]
 
-    # cria dicionário vazio
-    TT = {}
-    # faz o for na lista 1
-    for k in lista01:
-        k = limpar_frase(k).lower()
-        # verifica o número de vezes que esse título existe e adiciona no dicionário
-        if k not in TT:
-            TT[k] = {"LISTA1": 1}
-        else:
-            TT[k]["LISTA1"] = TT[k]["LISTA1"] + 1
-    # faz o for na lista 2
-    for k in lista02:
-        k = limpar_frase(k).lower()
-        # verifica se a lista 2 está presente no dicionário e adiciona qntas vezes est[a
-        if k not in TT:
-            TT[k] = {"LISTA2": 1}
-        else:
-            if "LISTA2" not in TT[k]:
-                TT[k]["LISTA2"] = 0
-            TT[k]["LISTA2"] = TT[k]["LISTA2"] + 1
-    # cria lista vazia para adicionar os arquivos
-    lista_novos_artigos = []
-    # se está na lista 1 e não está na 2 trata-se de um artigo novo
-    for k, v in TT.items():
-        if "LISTA1" in v and "LISTA2" not in v:
-            for i in range(0, TT[k]['LISTA1']):
-                artigo =[]
-                artigo.append(k)
-                lista_novos_artigos.append(artigo)
-            print(TT[k]['LISTA1'])
-            i += TT[k]['LISTA1']
-    # se está na lista 1 e não está na 2 trata-se de um artigo que não aparece mais na string de busca
-    for k, v in TT.items():
-        if "LISTA2" in v and "LISTA1" not in v:
-            i -= TT[k]['LISTA2']
-    # se o mesmo nome aparece 2 vezes na base 1 e passa a aparecer 4.
-    #  O artigo aumentou o número de repetições
-    for k,v in TT.items():
-        if "LISTA2" in v and "LISTA1" in v:
-            atigos_que_aumenta_o_numero_de_repeticoes = TT[k]["LISTA1"] - TT[k]['LISTA2']
-            if atigos_que_aumenta_o_numero_de_repeticoes > 0:
-                print(f'O artigo {k} passa a aparecer  {atigos_que_aumenta_o_numero_de_repeticoes} vezes ')
-                i += atigos_que_aumenta_o_numero_de_repeticoes
-                for i in range(0, atigos_que_aumenta_o_numero_de_repeticoes):
-                    artigo = []
-                    artigo.append(k)
-                    lista_novos_artigos.append(artigo)
+    # Filtra as colunas selecionadas e converte em uma lista de listas
+    SCOPUS2024 = ref01[colunas_selecionadas].values.tolist()
+    SCOPUS2021 = ref02[colunas_selecionadas].values.tolist()
 
-
-
-
-
-    #print(lista_novos_artigos)
-    print("count",i)
-    exporta_lista_para_csv(lista_novos_artigos, "novos_arquivos_scopus.csv")
+    dicionario_comparacao_SCOPUS = compara_duas_listas(SCOPUS2024, SCOPUS2021)
+    lista_novos_artigos_SCOPUS = retorna_lista_delta(dicionario_comparacao_SCOPUS)
+    exporta_lista_para_csv(lista_novos_artigos_SCOPUS, "novos_arquivos_scopus.csv")
